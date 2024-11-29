30b280a3ddd50ff864bc38379066cd3b1125e2a0
JonathanPerales417 <peralesj940@gmail.com>
2024-11-29 15:52:07 -0600

    Add files via upload

--- ticket.php
+++ ticket.php
@@ -1,4 +1,8 @@
 <?php 
+session_start();
+$usuario = $_SESSION["usuario"];
+$email = $_SESSION["email"];
+
    require('rounded_rect.php');
    setlocale(LC_TIME, "Spanish_Mexican");
 
@@ -12,13 +16,16 @@
    $pdf->Cell(285,4,'Bueno por:',0,1,'C');
    $pdf->Image('../Ticket.png',10,5,180,300);
    $pdf->Ln(45);
-   $pdf->Cell(80,10,date("Y-m-d"),0,1,'R');
+    $pdf->Cell(80,10,date("Y-m-d"),0,1,'R');
+    $pdf->Cell(100,40,$usuario,0,1,'R');
+
     if(file_exists('../carritodecompras.txt')){
       $content = trim(file_get_contents('../carritodecompras.txt'), PHP_EOL);
       $lineas = explode(PHP_EOL, $content);
       $total = 0;
-      $pdf->Cell(30,70,' ', 0,1, 'R');
 
+      $pdf->Cell(30,30,' ', 0,1, 'R');
+      $total = 0;
       foreach($lineas as $linea){
          list($productoE, $precioE) = explode(',', $linea);
          $pdf->Cell(120,10,"    1     " . $productoE, 0,1, 'L');
@@ -26,10 +33,12 @@
          $pdf->Cell(30,10,' ', 0,1, 'R');
          $total = $total + $precioE;
       }
-       $pdf->Ln(93);
-   $pdf->Cell(160,10,date("Y-m-d"),0,1,'R');
+      $pdf->Ln(99);
+     $pdf->Cell(160,10,"$ " . $total, 0,1, 'R');   
    }
+
    $pdf->Output();
+
    //Aqui se borra el Carrito de Compras
-if(file_exists('../carritocompras.txt')) unlink("../carritocompras.txt");
+   if(file_exists('../carritocompras.txt')) unlink("../carritocompras.txt");
 ?>
